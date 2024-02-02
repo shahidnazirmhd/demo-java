0d899b5d276b35b07d5d1e24319522fd7000b5c3
Shahid <shahidnazirmhd@gmail.com>
2024-02-02 11:40:06 +0530

    array demo 2

--- src/com/snm/demo/arrays/ArrayDemo4.java
+++ src/com/snm/demo/arrays/ArrayDemo4.java
@@ -3,33 +3,93 @@ Write a Java program to print all the LEADERS in the array.
 Note: An element is leader if it is greater than all the elements to its right side.
 And the rightmost element is always a leader.
  */
+
+/*
+It can also be achieved by two methods
+*/
 package com.snm.demo.arrays;
+
+import java.util.Scanner;
 
 public class ArrayDemo4 {
 
     public static void printLeaders(int[] arr) {
-        int n = arr.length;
-        int maxFromRight = arr[n - 1];
-
-        // The last element is always a leader
-        System.out.println("Leaders in the array:");
-        System.out.print(maxFromRight + " ");
-
-        // Traverse the array from right to left
-        for (int i = n - 2; i >= 0; i--) {
-            if (arr[i] > maxFromRight) {
-                maxFromRight = arr[i];
-                System.out.print(maxFromRight + " ");
+       int size = arr.length;
+        System.out.println(size);
+//       int leader = arr[size-1];
+//       System.out.print("Leaders are: " + leader);
+//       for(int i = size - 2; i >= 0; i--) {
+//          if(arr[i] > leader) {
+//              leader = arr[i];
+//              System.out.print(" " + leader);
+//          }
+//        }
+        for (int i = 0; i < size; i++) {
+            int j;
+            for (j = i + 1; j < size; j++) {
+                if(arr[i] <= arr[j]) {
+                    break;
+                }
+            }
+            if(j == size){
+                System.out.print(arr[i] + " ");
             }
         }
+
     }
 
     public static void main(String[] args) {
-        int[] arr = {16, 17, 4, 3, 5, 2};
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of the number array:");
+        int len = scan.nextInt();
+        int[] arr = new int[len];
+        System.out.println("Enter values:");
+        for (int i = 0; i < len; i++) {
+            arr[i] = scan.nextInt();
+        }
         printLeaders(arr);
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
-It can be achieved by using two loops
+for (int i = 0; i < size; i++)
+        {
+            int j;
+            for (j = i + 1; j < size; j++)
+            {
+                if (arr[i] <=arr[j])
+                    break;
+            }
+            if (j == size) // the loop didn't break
+                System.out.print(arr[i] + " ");
+        }
  */
